a5655e3be75988ac2b71cee515e5e9a8e5fe2818
YelemyahM <yelemyah.muamba@gmail.com>
2020-03-21 18:42:06 +0100

    refactor: refactoring of code

--- assets/lib/script.js
+++ assets/lib/script.js
@@ -1,20 +1,26 @@
+// Variables
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-30;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
-// Draw mouvement's ball
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+// The ball
+function drawBall(){
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI * 2);
+	ctx.arc(x, y, 10, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
+}
+
+// Ball mouvements
+function ballMouvment() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
 	x += dx;
 	y += dy;
 }
 
-setInterval(draw, 15);
+setInterval(ballMouvment, 10);
