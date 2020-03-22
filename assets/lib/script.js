bf8dfebc3463dd0da23be2034bafec380e7a4c26
YelemyahM <yelemyah.muamba@gmail.com>
2020-03-22 10:08:05 +0100

    docs: ball bouncing

--- assets/lib/script.js
+++ assets/lib/script.js
@@ -1,4 +1,4 @@
-// Variables
+/* Variables */
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
@@ -7,7 +7,7 @@ var dx = 2;
 var dy = -2;
 var ballRadius = 10;
 
-// The ball
+/* The ball */
 function drawBall(){
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -16,15 +16,16 @@ function drawBall(){
   ctx.closePath();
 }
 
-// Ball mouvements
+/* Ball mouvements */
 function ballMouvment() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
 
+  // Bounce on vertical walls
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-
+  // Bounce on horizontal walls
   if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
   }
