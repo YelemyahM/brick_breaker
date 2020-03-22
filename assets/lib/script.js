1761412afc714bbca7ed1f01b1856a93220c68c4
YelemyahM <yelemyah.muamba@gmail.com>
2020-03-22 12:01:13 +0100

    feat: Paddle and keybord controls

--- assets/lib/script.js
+++ assets/lib/script.js
@@ -1,25 +1,61 @@
 /* Variables */
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-10;
+var x = canvas.width / 2;
+var y = canvas.height - 10;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", KeyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+/* Key press Down */
+function KeyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+/* Key press up */
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
+  }
+}
 
 /* The ball */
-function drawBall(){
-	ctx.beginPath();
-	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-	ctx.fillStyle = "#0095DD";
-	ctx.fill();
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+/* The paddle */
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
   ctx.closePath();
 }
 
 /* Ball mouvements */
 function ballMouvment() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
 
   // Bounce on vertical walls
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
@@ -30,8 +66,17 @@ function ballMouvment() {
     dy = -dy;
   }
 
-	x += dx;
-	y += dy;
+  // right paddle mouvements
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  }
+  // left paddle mouvements
+  else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
+  x += dx;
+  y += dy;
 }
 
 setInterval(ballMouvment, 20);
