c92b1a4e27b839f682c2a3544f842085db18472a
YelemyahM <yelemyah.muamba@gmail.com>
2020-03-22 09:58:34 +0100

    feat: bounce of the walls

--- assets/lib/script.js
+++ assets/lib/script.js
@@ -1,26 +1,36 @@
 // Variables
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width / 2;
-var y = canvas.height - 30;
+var x = canvas.width/2;
+var y = canvas.height-10;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
 // The ball
 function drawBall(){
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
-	ctx.closePath();
+  ctx.closePath();
 }
 
 // Ball mouvements
 function ballMouvment() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawBall();
+  drawBall();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
 	x += dx;
 	y += dy;
 }
 
-setInterval(ballMouvment, 10);
+setInterval(ballMouvment, 20);
