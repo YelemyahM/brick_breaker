27bb5d1b2a01ae0b92f323e51b99954d523f63c9
YelemyahM <yelemyah.muamba@gmail.com>
2020-03-26 16:08:06 +0100

    feat: detection collision

--- assets/lib/script.js
+++ assets/lib/script.js
@@ -24,7 +24,7 @@ var bricks = [];
 for (var c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   for (var r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0 };
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
   }
 }
 
@@ -46,6 +46,18 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if (e.keyCode == 37) {
     leftPressed = false;
+  }
+}
+
+function collisonDetection(){
+  for(var c = 0; c < brickColumnCount; c++){
+    for(var r = 0; r < brickRowCount; r++){
+      var b = bricks[c][r];
+      if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+        dy = -dy;
+        b.status = 0;
+      }
+    }
   }
 }
 
@@ -71,15 +83,17 @@ function drawPaddle() {
 function drawBricks() {
   for (var c = 0; c < brickColumnCount; c++) {
     for (var r = 0; r < brickRowCount; r++) {
-      var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-      var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+      if (bricks[c][r].status == 1) {
+        var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
@@ -90,6 +104,7 @@ function ballMouvment() {
   drawBricks();
   drawBall();
   drawPaddle();
+  collisonDetection();
 
   // Bounce on vertical walls
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
